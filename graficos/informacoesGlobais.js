3c009cb82d27feac206d3553d4d1e58f8d687f59
daysla2629 <daysla.silva@escola.pr.gov.br>
2024-10-29 12:10:25 -0300

    l,lk

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,6 +7,10 @@ async function visualizarInformacoesGlobais() {
         const dados = await res.json()
 
         console.log(dados);
+        const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Hoje, existem cerca de${dados.total_pessoas_mundo}, conctadas em redes sociais são  ${dados.total_pessoas_conectadas} da população. Em média, os usúarios passam cerca de ${dados.tempo_medio} por dia nessas plataformas.`
+}
 
     }
 
