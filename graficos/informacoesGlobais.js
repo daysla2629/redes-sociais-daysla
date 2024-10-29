277701c9648b1853765f65723bba6b0430141890
daysla2629 <daysla.silva@escola.pr.gov.br>
2024-10-29 12:16:16 -0300

    f

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,7 +9,7 @@ async function visualizarInformacoesGlobais() {
         console.log(dados);
         const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
-paragrafo.innerHTML = `Hoje, existem cerca de${dados.total_pessoas_mundo}, conctadas em redes sociais são  ${dados.total_pessoas_conectadas} da população. Em média, os usúarios passam cerca de ${dados.tempo_medio} por dia nessas plataformas.`
+paragrafo.innerHTML = `Hoje, existem cerca de <span>${dados.total_pessoas_mundo}, conctadas em redes sociais são <span> ${dados.total_pessoas_conectadas} da população. Em média, os usúarios passam cerca de <span> ${dados.tempo_medio} por dia nessas plataformas.`
 const container = document.getElementById(‘graficos-container’)
 container.appendChild(paragrafo)
 }
