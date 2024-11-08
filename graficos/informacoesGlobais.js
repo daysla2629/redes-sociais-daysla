f146d8b28b544f44ff69af7ddd21bb612748e82f
daysla2629 <daysla.silva@escola.pr.gov.br>
2024-11-08 07:56:55 -0300

    chf

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,17 +3,16 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+   
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
 
-    paragrafo.innerHTML = `Hoje, existem cerca de <span> ${dados.total_pessoas_mundo}, conctadas em redes sociais são <span>${dados.total_pessoas_conectadas} da população. Em média, os usúarios passam cerca de <span>${dados.tempo_medio} por dia nessas plataformas.`
+    paragrafo.innerHTML = `Hoje, existem cerca de <span>${dados.total_pessoas_mundo}</span>, conctadas em redes sociais são <span>${dados.total_pessoas_conectadas}</span> da população. Em média, os usúarios passam cerca de <span>${dados.tempo_medio}</span> por dia nessas plataformas.`
     console.log(paragrafo)
 
    const container = document.getElementById(‘graficos-container’)
    container.appendChild(paragrafo)
 
 
-
-    visualizarInformacoesGlobais()
 }
+    visualizarInformacoesGlobais()
