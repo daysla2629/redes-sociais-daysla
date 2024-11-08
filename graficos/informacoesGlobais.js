a8b12c867ac0c61fdec5a199dae583f29cbe0da8
daysla2629 <daysla.silva@escola.pr.gov.br>
2024-11-08 07:38:44 -0300

    jejf

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,11 +1,11 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoesGlobais() {
-      const res = await fetch(url)
-      const dados = await res.json()
-      console.log(dados);
-      const paragrafo = document.createElement('p')
-      paragrafo.classList.add('graficos-container__texto')
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
 
   paragrafo.innerHTML = `Hoje, existem cerca de <span> ${dados.total_pessoas_mundo}, conctadas em redes sociais são <span>${dados.total_pessoas_conectadas} da população. Em média, os usúarios passam cerca de <span>${dados.tempo_medio} por dia nessas plataformas.`
   console.log(paragrafo)
