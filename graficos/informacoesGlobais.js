aba120dc3966edd2057a1dd4b5949fcb2060caf2
daysla2629 <daysla.silva@escola.pr.gov.br>
2024-11-25 08:49:22 -0300

    jnsjhwdb

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,12 +3,15 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-
-    paragrafo.innerHTML = `Hoje, existem cerca de <span>${dados.total_pessoas_mundo}</span> conctadas em redes sociais são <span>${dados.total_pessoas_conectadas}</span> da população. Em média, os usúarios passam cerca de <span>${dados.tempo_medio}</span> por dia nessas plataformas.`
-    console.log(paragrafo)
+    paragrafo.innerHTML = `Hoje, existem cerca de <span>${pessoasNoMundo} bilhões</span> conctadas em redes sociais são <span>${pessoasConectadas} bilhões</span> estão conectadas em rede social e passam  <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>a porcentagem de pessoas é de <span>${porcentagemConectada}%</span> da população. Em média, os usúarios passam cerca de <span>${dados.tempo_medio}</span> por dia.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
